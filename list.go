d3bb44660a457fa07781bda881de4f8f463e6c30
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-03 23:25:39 +0900

    :books: add: docs

--- list.go
+++ list.go
@@ -2,12 +2,16 @@ package pythonlike_go
 
 import "fmt"
 
+//List is python like object in Golang
 type List []interface{}
 
+//NewList create new list
 func NewList() List {
 	return List{}
 }
 
+//Index return zero-based index in the list of the first item whose value is equal to x.
+//return -1 if there is no such item.
 func (l List) Index(element interface{}) int {
 	for k, v := range l {
 		if element == v {
@@ -17,10 +21,12 @@ func (l List) Index(element interface{}) int {
 	return -1
 }
 
+// Add an item to the end of the list.
 func (l *List) Append(element interface{}) {
 	*l = append(*l, element)
 }
 
+//Remove the first item from the list whose value is equal to x. It returns an error if there is no such item.
 func (l *List) Remove(element interface{}) error {
 	idx := l.Index(element)
 	if idx == -1 {
@@ -29,16 +35,22 @@ func (l *List) Remove(element interface{}) error {
 	return l.removeByIndex(idx)
 }
 
+//removeByIndex remove selected index from list
 func (l *List) removeByIndex(idx int) error {
 	list := []interface{}(*l)
 	*l = append(list[:idx], list[idx+1:]...)
 	return nil
 }
 
+//Length get the length of list
 func (l List) Length() int {
 	return len(l)
 }
 
+//Insert an item at a given position.
+//The first argument is the index of the element before which to insert,
+//so a.Insert(0, x) inserts at the front of the list,
+//and a.Insert(len(a), x) is equivalent to a.Append(x).
 func (l *List) Insert(idx int, element interface{}) error {
 	if l.Length() < idx || idx < 0 {
 		return fmt.Errorf("index out of range")
@@ -52,6 +64,8 @@ func (l List) getLastIndex() int {
 	return l.Length() - 1
 }
 
+//Pop remove the item at the given position in the list, and return it.
+//If no index is specified, a.Pop() removes and returns the last item in the list.
 func (l *List) Pop(idx ...int) (interface{}, error) {
 	index := l.getLastIndex()
 	ll := len(idx)
@@ -75,10 +89,12 @@ func (l *List) Pop(idx ...int) (interface{}, error) {
 	return element, l.removeByIndex(index)
 }
 
+//Clear remove all items from the list
 func (l *List) Clear() {
 	*l = NewList()
 }
 
+//Count Return the number of times x appears in the list.
 func (l List) Count(element interface{}) int {
 	count := 0
 	for _, v := range l {
@@ -89,6 +105,7 @@ func (l List) Count(element interface{}) int {
 	return count
 }
 
+//Copy Return a shallow copy of the list
 func (l List) Copy() List {
 	return l
 }
