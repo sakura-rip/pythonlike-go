80dd0be5a8ebe4265ac1292f9fa215b59e52771f
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-03 16:58:42 +0900

    :heavy_plus_sign: add: List object

---
+++ list.go
@@ -0,0 +1,41 @@
+package pythonlike_go
+
+import "fmt"
+
+type List []interface{}
+
+func (l List) Index(element interface{}) int {
+	for k, v := range l {
+		if element == v {
+			return k
+		}
+	}
+	return -1
+}
+
+func (l *List) Append(element interface{}) {
+	*l = append(*l, element)
+}
+
+func (l *List) Remove(element interface{}) error {
+	idx := l.Index(element)
+	if idx == -1 {
+		return fmt.Errorf("element not found")
+	}
+	list := []interface{}(*l)
+	*l = append(list[:idx], list[idx+1:]...)
+	return nil
+}
+
+func (l List) Length() int {
+	return len(l)
+}
+
+func (l *List) Insert(idx int, element interface{}) error {
+	if l.Length() < idx {
+		return fmt.Errorf("index out of range")
+	}
+	list := []interface{}(*l)
+	*l = append(list[:idx], append([]interface{}{element}, list[idx:]))
+	return nil
+}
