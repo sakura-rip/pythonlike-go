ee1e497cdaab8db99af501caed3b46d9dd6b00fb
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-03 23:26:25 +0900

    :bug: fix: docs

--- list.go
+++ list.go
@@ -50,7 +50,7 @@ func (l List) Length() int {
 //Insert an item at a given position.
 //The first argument is the index of the element before which to insert,
 //so a.Insert(0, x) inserts at the front of the list,
-//and a.Insert(len(a), x) is equivalent to a.Append(x).
+//and a.Insert(len(a)-1, x) is equivalent to a.Append(x).
 func (l *List) Insert(idx int, element interface{}) error {
 	if l.Length() < idx || idx < 0 {
 		return fmt.Errorf("index out of range")
