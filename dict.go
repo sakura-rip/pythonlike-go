c12b7662214b02616253b3858ede437fced97223
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-03 23:56:03 +0900

    :recycle: change: type name Map to dict

---
+++ dict.go
@@ -0,0 +1,87 @@
+package pythonlike_go
+
+//List is python like object dictionary in Golang
+type Dict map[interface{}]interface{}
+
+func NewMap() Dict {
+	return Dict{}
+}
+
+//Return the number of items in the dictionary d.
+func (d Dict) Length() int {
+	return len(d)
+}
+
+//Return a new view of the dictionary’s keys
+func (d Dict) Keys() []interface{} {
+	var keys []interface{}
+	for k := range d {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+//Remove all items from the dictionary
+func (d *Dict) Clear() {
+	*d = NewMap()
+}
+
+//Check is the key exist
+func (d Dict) IsKeyExist(key interface{}) bool {
+	_, ok := d[key]
+	return ok
+}
+
+//Return the value for key if key is in the dictionary, else default.
+func (d Dict) Get(key, dv interface{}) interface{} {
+	val, ok := d[key]
+	if !ok {
+		return dv
+	}
+	return val
+}
+
+func (d Dict) Pop(key, dv interface{}) interface{} {
+	val, ok := d[key]
+	if ok {
+		delete(d, key)
+		return val
+	}
+	return dv
+}
+
+//Remove and return a (key, value) pair from the dictionary.
+func (d Dict) PopItem() (interface{}, interface{}) {
+	for k, v := range d {
+		delete(d, k)
+		return k, v
+	}
+	return nil, nil
+}
+
+//If key is in the dictionary, return its value.
+//If not, insert key with a value of default and return default. default defaults to Nil.
+func (d Dict) SetDefault(key, value interface{}) interface{} {
+	if d.IsKeyExist(key) {
+		return d[key]
+	}
+	d[key] = value
+	return value
+}
+
+//Update the dictionary with the key/value pairs from other, overwriting existing keys.
+//update() accepts either another dictionary object
+func (d Dict) Update(map2 Dict) {
+	for k, v := range map2 {
+		d[k] = v
+	}
+}
+
+//Return a new view of the dictionary’s values
+func (d Dict) Values() []interface{} {
+	var slice []interface{}
+	for _, v := range d {
+		slice = append(slice, v)
+	}
+	return slice
+}
---
+++ dict_test.go
@@ -0,0 +1,59 @@
+package pythonlike_go
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var (
+	ma Dict
+)
+
+func init() {
+	ma = NewMap()
+	ma["hoge"] = "hage"
+	ma["fifi"] = "fasjfa"
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	fmt.Println(ma)
+	os.Exit(code)
+}
+
+func TestMapLength(t *testing.T) {
+	length := ma.Length()
+	fmt.Println(length)
+}
+func TestMapKeys(t *testing.T) {
+	keys := ma.Keys()
+	fmt.Println(keys)
+}
+func TestMapIsKeyExist(t *testing.T) {
+	exist := ma.IsKeyExist("hoge")
+	fmt.Println(exist)
+}
+func TestMapGet(t *testing.T) {
+	get := ma.Get("fasdfa", nil)
+	fmt.Println(get)
+}
+func TestMapPop(t *testing.T) {
+	pop := ma.Pop("hoge", nil)
+	fmt.Println(pop)
+}
+func TestMapPopItem(t *testing.T) {
+	item, i := ma.PopItem()
+	fmt.Println(item, i)
+}
+func TestMapSetDefault(t *testing.T) {
+	ma.SetDefault("kasu", Dict{"hi": "f"})
+}
+
+func TestMapUpdate(t *testing.T) {
+	ma.Update(Dict{"test": "gogo"})
+}
+
+func TestMapValues(t *testing.T) {
+	ma.Values()
+}
