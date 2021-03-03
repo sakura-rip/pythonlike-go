1a68ef4cc3706636ab45726557cee12a948cb2d3
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-03 17:36:31 +0900

    :white_check_mark: add: some test

--- list_test.go
+++ list_test.go
@@ -31,3 +31,38 @@ func TestList_Insert3(t *testing.T) {
 	}
 	fmt.Println(list)
 }
+
+func TestList_Pop(t *testing.T) {
+	list := List{"hoge", "hage"}
+	v, err := list.Pop()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(v)
+	fmt.Println(list)
+}
+
+func TestList_Pop2(t *testing.T) {
+	list := List{"hoge", "hage"}
+	v, err := list.Pop(1)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(v)
+	fmt.Println(list)
+}
+
+func TestList_Clear(t *testing.T) {
+	list := List{"hoge", "hage"}
+	list.Clear()
+	fmt.Println(list)
+}
+
+func TestList_Copy(t *testing.T) {
+	list := List{"hoge", "hage"}
+	fmt.Println(list)
+	newList := list.Copy()
+	fmt.Println(newList)
+	newList.Clear()
+	fmt.Println(newList)
+}
